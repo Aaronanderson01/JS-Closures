3fe757cd16edad16990e0058fcba6f82f4d6fba2
Aaronanderson01 <aaronanderson01@gmail.com>
2016-09-01 18:05:30 -0600

    initial commit

--- closures.js
+++ closures.js
@@ -11,12 +11,12 @@ var outer = function(){
 // Above you're given a function that returns another function which has a closure over the name variable.
 // Invoke outer saving the return value into another variable called 'inner'.
 
-// Code Here
+var inner = outer();
 
 
 //Once you do that, invoke inner.
 
-  //Code Here
+  inner();
 
 
 
@@ -35,7 +35,9 @@ var callFriend = function(){
 // Above you're given a callFriend function that returns another function.
 // Create a makeCall function that when invoked logs  'Calling Jake at 435-215-9248' in your console.
 
-  //Code Here
+var makeCall = callFriend();
+
+console.log(makeCall("435-215-9248"));
 
 
 
@@ -51,14 +53,16 @@ var callFriend = function(){
   Write a function called makeCounter that makes the following code work properly.
 */
 
-//Code Here
+function makeCounter(num) {
+  return function(){
+    for(var i=1; i<5; i++) {
+    console.log(i);
+    }
+  };
+}
+  var count = makeCounter();
 
-//Uncomment this once you make your function
-//   var count = makeCounter();
-//   count(); // 1
-//   count(); // 2
-//   count(); // 3
-//   count(); // 4
+count();
 
 
 
@@ -72,16 +76,20 @@ var callFriend = function(){
 // You will need to use the module pattern to achieve this.
 
 function counterFactory(value) {
-
-  // Code here.
-
-
-  return {
-  }
+  var inc = function() {
+  return value + 1;
+  };
+  var dec = function() {
+    return value - 1;
+  };
+  return {inc:inc, dec:dec};
 }
 
 
-counter = counterFactory(10);
+ counter = counterFactory(10);
+
+console.log(counter.inc());
+console.log(counter.dec());
 
 
 
@@ -90,74 +98,63 @@ counter = counterFactory(10);
 
 
 // Inside the motivation function create another function called message that will return 'You're doing awesome, keep it up firstname lastname.'
-
-  function motivation(firstname, lastname){
+function motivation(firstname, lastname){
 
     var welcomeText = 'You\'re doing awesome, keep it up ';
+    var message = function() {
+      return welcomeText + firstname + " " + lastname;
+    };
+  return message();
+}
 
-    // code message function here.
 
 
-    //Uncommment this to return the value of your invoked message function
-
-    //return message()
-  }
-
-  motivation('Billy', 'Bob'); // 'Your doing awesome keep it up Billy Bob
+ console.log(motivation('Billy', 'Bob'));
 
 
 
 //////////////////PROBLEM 6////////////////////
 
-// Inside the return create a publicMethod property that is a function that invokes privateMethod. After you create the privateMethod
-// Invoke it by calling module.publicMethod(); outside the module scope
+var module = (function() {
+  var person = {
+    name: "phillip",
+    age: 29,
+    location: 'Utah'
+  };
 
-  var module = (function() {
-    var person = {
-      name: "phillip",
-      age: 29,
-      location: 'Utah'
+  var privateMethod = function(){
+    return "Hi, I'm " + person.name + ", age " + person.age + " from " + person.location;
+  };
+
+  // Anything that is being returned is made public and can be invoked from outside our lexical scope
+
+  return {
+    publicMethod: function() {
+     return privateMethod();
+    }
     };
 
-    var privateMethod = function(){
-      return "Hi, I'm " + person.name + ", age " + person.age + " from " + person.location;
-    };
+})();
 
-    // Anything that is being returned is made public and can be invoked from outside our lexical scope
 
-    return {
-      // Code here.
-    };
-
-  })();
-
-//Uncomment this after you create your public method
-//   module.publicMethod();
-
+console.log(module.publicMethod());
 
 
 //////////////////PROBLEM 7////////////////////
-// Here we have a for loop that will iterate as long as i is less than or equal to 5. What we need to do is console.log(i)
-// So that it logs ( 0 then 1 then 2 then 3, etc). Run this code in your console to see what the output is.
-
 
 function timeOutCounter() {
   for (var i = 0; i <= 5; i++) {
-    setTimeout(function() {
-      console.log(i);
-    }, i * 1000)
+    setTimeout(newScope(i), i * 1000);
   }
 
   function newScope(i) {
-    console.log(i)
+
+    return function() {
+      console.log(i);
+    };
   }
 }
 timeOutCounter();
-  // To make this code work you will need to create a new scope for every iteration.
-
-
-
-
 
 //////////////////PROBLEM 8////////////////////
 
